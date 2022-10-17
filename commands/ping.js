f39bd67913526ed05904f92a0c40c014dc4f6c13
zockol <pz113634@fh-muenster.de>
2022-10-17 20:40:13 +0200

    bot posts "Welcome" on user server join

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,4 @@ const {SlashCommandBuilder} = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
-	async execute(interaction) {
-		await interaction.reply("Pong!");
-	},
 };
--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const {Client, GatewayIntentBits, Collection} = require("discord.js");
 const env = require("dotenv").config();
 const deployCommands = require("./functions/SlashCommands/deploy-commands.js");
 const interactionCreate = require("./functions/Interactions/interactionCreate.js");
+const fs = require("fs");
 
-const client = new Client({intents: GatewayIntentBits.Guilds});
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+});
+
+const pathSettings = "\\jsons\\settings.json";
+var readSettings = fs.readFileSync(__dirname + pathSettings);
+var fileSettings = JSON.parse(readSettings);
 
 client.on("ready", () => {
 	console.log("Bot is online");
@@ -12,6 +19,13 @@ client.on("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
 	interactionCreate(interaction);
+});
+
+client.on("guildMemberAdd", (member) => {
+	console.log("trest");
+	if (fileSettings[0].welcomeMessage == 1) {
+		client.channels.cache.get(fileSettings[0].Channel).send(`Welcome`);
+	}
 });
 
 client.login(process.env.TOKEN);
