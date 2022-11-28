a1c62a8bb3a48305ffc98f9590f31e34558bfa43
zockol <pz113634@fh-muenster.de>
2022-11-28 18:28:25 +0100

    basic activity tracking in seconds

--- functions/voiceEvents/voiceActivity.js
+++ functions/voiceEvents/voiceActivity.js
@@ -8,10 +8,27 @@ module.exports = function (oldMember, newMember) {
 
     var userTAG = newMember.member.user.tag
     var userID = newMember.id
+    var DateInSeconds = Math.floor(Date.now() / 1000)
 
     if (!File[userID]) {
-        File[userID] = {Name: userTAG, Time: 0, LastJoinedTimeStamp: Math.floor(Date.now() / 1000)};
+        File[userID] = {Name: userTAG, Time: 0, LastJoinedTimeStamp: DateInSeconds};
+    } else {
+        if (newMember.channelId != oldMember.channelId) {
+
+            if (oldMember.channelId != null && File[userID].LastJoinedTimeStamp != 0) {
+                File[userID].Time += (DateInSeconds - File[userID].LastJoinedTimeStamp)
+            }
+            
+            if (newMember.channelId != null) {
+                File[userID].LastJoinedTimeStamp = DateInSeconds
+            } else {
+                File[userID].LastJoinedTimeStamp = 0
+            }
+            
+        }
     }
+
+
 
     fs.writeFileSync(__dirname + Path, JSON.stringify(File, null, 2));
 };
